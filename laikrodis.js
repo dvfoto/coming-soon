1cbb4ee313d829fc3b585afc2ef212521ccb0d59
Deividas <vilimaitisdeividas@gmail.com>
2022-04-05 09:33:59 +0300

    laikrodis2

--- laikrodis.js
+++ laikrodis.js
@@ -31,13 +31,13 @@
             document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
             document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
   
-          //do something later when date is reached
-          if (distance < 0) {
-            document.getElementById("headline").innerText = "It's my birthday!";
-            document.getElementById("countdown").style.display = "none";
-            document.getElementById("content").style.display = "block";
-            clearInterval(x);
-          }
-          //seconds
+          // //do something later when date is reached
+          // if (distance < 0) {
+          //   document.getElementById("headline").innerText = "It's my birthday!";
+          //   document.getElementById("countdown").style.display = "none";
+          //   document.getElementById("content").style.display = "block";
+          //   clearInterval(x);
+          // }
+          // //seconds
         }, 0)
     }());
