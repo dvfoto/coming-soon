0cdfe64d327461d4f5ff63c13f0dd2db07c9e47e
Deividas <vilimaitisdeividas@gmail.com>
2022-04-05 13:03:26 +0300

    barai

--- laikrodis.js
+++ laikrodis.js
@@ -11,7 +11,7 @@
         mm = String(today.getMonth() + 1).padStart(2, "0"),
         yyyy = today.getFullYear(),
         nextYear = yyyy + 1,
-        dayMonth = "09/30/",
+        dayMonth = "03/30/",
         birthday = dayMonth + yyyy;
     
     today = mm + "/" + dd + "/" + yyyy;
